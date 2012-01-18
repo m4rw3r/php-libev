c42734eb950b9095154b996b6eb78274789b7a36
m4rw3r <m4rw3r@gmail.com>
2012-01-19 00:23:56 +0100

    Added the Simplified BSD License to to the source

--- php_libev.h
+++ php_libev.h
@@ -1,3 +1,32 @@
+
+/*
+ * Copyright (c) 2012 Martin Wernståhl <m4rw3r@gmail.com>. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ * 
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ * 
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY Martin Wernståhl ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Martin Wernståhl OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Martin Wernståhl.
+ */
+
 #ifndef PHP_LIBEV_H
 #define PHP_LIBEV_H 1
 
@@ -10,9 +39,5 @@ extern zend_module_entry libev_module_entry;
 #ifdef ZTS
 #include "TSRM.h"
 #endif
-
-// Not sure if needed, but makes it easier to create Namespaced classes/methods
-#define ZEND_NS_ME(ns, classname, name, arg_info, flags)	ZEND_NS_FENTRY(ns, name, ZEND_MN(classname##_##name), arg_info, flags)
-
 
 #endif /* PHP_LIBEV_H */
